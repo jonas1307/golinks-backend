f6c54e8a698e2776a894e7bcaf97dd12934ed73f
Jonas Lima de Amorim <dev.amorim@outlook.com>
2022-10-21 20:37:34 -0300

    fix: correcting references for application startup

--- Golinks.Repository/Extensions/ServiceCollectionExtension.cs
+++ Golinks.Repository/Extensions/ServiceCollectionExtension.cs
@@ -2,6 +2,7 @@
 using Golinks.Repository.Contracts;
 using Golinks.Repository.Extensions.Settings;
 using Golinks.Repository.Repositories;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
@@ -9,22 +10,15 @@ namespace Golinks.Repository.Extensions;
 
 public static class ServiceCollectionExtension
 {
-    public static void RegisterRepositoryServices(this IServiceCollection services, Action<MongoDbSettings> configureOptions)
+    public static void RegisterRepositoryServices(this IServiceCollection services, IConfiguration configuration)
     {
-        if (services == null)
-        {
-            throw new ArgumentNullException(nameof(services));
-        }
-
-        if (configureOptions == null)
-        {
-            throw new ArgumentNullException(nameof(configureOptions));
-        }
+        if (services == null) throw new ArgumentNullException(nameof(services));
+        if (configuration == null) throw new ArgumentNullException(nameof(configuration));
 
         services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
         services.AddTransient<ILinkRepository, LinkRepository>();
 
-        services.Configure(configureOptions);
+        services.Configure<MongoDbSettings>(configuration.GetSection("MongoDbSettings"));
 
         services.AddSingleton<IMongoDbSettings>(serviceProvider =>
             serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value);
