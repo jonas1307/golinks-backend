11b00972c9007077643c2debd8488d55d547b232
Jonas Lima de Amorim <dev.amorim@outlook.com>
2022-10-25 19:37:03 -0300

    feat: implementing metric controller

---
+++ Golinks.Application/MappingProfiles/MetricProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Golinks.Application.ViewModel;
+using Golinks.Domain.Entities;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Golinks.Application.MappingProfiles;
+
+[ExcludeFromCodeCoverage]
+public class MetricProfile : Profile
+{
+    public MetricProfile()
+    {
+        CreateMap<Metric, MetricViewModel>();
+        CreateMap<MetricViewModel, Metric>();
+    }
+}
---
+++ Golinks.Application/ViewModel/MetricViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Golinks.Application.ViewModel
+{
+    public class MetricViewModel
+    {
+        public string? Id { get; set; } = null;
+        public string LinkId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
