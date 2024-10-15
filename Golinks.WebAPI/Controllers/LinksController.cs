2dc2ece4bb5d025f703ad14c62e89552152bf495
Jonas Lima de Amorim <dev.amorim@outlook.com>
2024-10-15 18:15:05 -0300

    refactor: reading from services instead of repository

--- Golinks.WebAPI/Controllers/LinksController.cs
+++ Golinks.WebAPI/Controllers/LinksController.cs
@@ -1,8 +1,8 @@
 ﻿using AutoMapper;
+using Golinks.Application.Contracts;
 using Golinks.Application.Requests;
 using Golinks.Application.ViewModel;
 using Golinks.Domain.Entities;
-using Golinks.Repository.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Golinks.WebAPI.Controllers;
@@ -10,9 +10,9 @@ namespace Golinks.WebAPI.Controllers;
 [ApiController]
 [Route("[controller]")]
 [Produces("application/json")]
-public class LinksController(ILinkRepository linkRepository, IMapper mapper) : Controller
+public class LinksController(ILinkService linkService, IMapper mapper) : Controller
 {
-    private readonly ILinkRepository _linkRepository = linkRepository;
+    private readonly ILinkService _linkSerice = linkService;
     private readonly IMapper _mapper = mapper;
 
     [HttpGet(Name = "GetAllLinks")]
@@ -20,7 +20,7 @@ public class LinksController(ILinkRepository linkRepository, IMapper mapper) : C
     [ProducesResponseType(typeof(IEnumerable<LinkViewModel>), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Index([FromQuery] LinkParams @params)
     {
-        var data = await _linkRepository.FindAllAsync(@params.PageNumber, @params.PageSize);
+        var data = await _linkSerice.FindAllAsync(@params.PageNumber, @params.PageSize);
 
         var links = _mapper.Map<IEnumerable<LinkViewModel>>(data);
 
@@ -32,7 +32,7 @@ public class LinksController(ILinkRepository linkRepository, IMapper mapper) : C
     [ProducesResponseType(typeof(IEnumerable<LinkViewModel>), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> GetById(Guid id)
     {
-        var data = await _linkRepository.FindByIdAsync(id);
+        var data = await _linkSerice.FindByIdAsync(id);
 
         if (data == null)
         {
@@ -54,7 +54,7 @@ public class LinksController(ILinkRepository linkRepository, IMapper mapper) : C
             return BadRequest();
         }
 
-        var linkInDb = await _linkRepository.FindOneAsync(f => f.Slug == model.Slug);
+        var linkInDb = await _linkSerice.FindOneAsync(f => f.Slug == model.Slug);
 
         if (linkInDb != null)
         {
@@ -63,7 +63,7 @@ public class LinksController(ILinkRepository linkRepository, IMapper mapper) : C
 
         var link = _mapper.Map<Link>(model);
 
-        await _linkRepository.CreateAsync(link);
+        await _linkSerice.CreateAsync(link);
 
         return CreatedAtAction(nameof(GetById), new { id = link.Id }, link);
     }
@@ -72,14 +72,14 @@ public class LinksController(ILinkRepository linkRepository, IMapper mapper) : C
     [ProducesResponseType(typeof(LinkViewModel), StatusCodes.Status200OK)]
     public async Task<IActionResult> Update(Guid id, [FromBody] LinkViewModel model)
     {
-        var linkInDb = await _linkRepository.FindOneAsync(x => x.Slug == model.Slug);
+        var linkInDb = await _linkSerice.FindOneAsync(x => x.Slug == model.Slug);
 
         if (linkInDb?.Id != id)
         {
             return BadRequest(new { Error = "Slug already in use" });
         }
 
-        linkInDb = await _linkRepository.FindByIdAsync(id);
+        linkInDb = await _linkSerice.FindByIdAsync(id);
 
         if (linkInDb == null)
         {
@@ -88,7 +88,7 @@ public class LinksController(ILinkRepository linkRepository, IMapper mapper) : C
 
         var link = _mapper.Map(model, linkInDb);
 
-        await _linkRepository.UpdateAsync(link);
+        await _linkSerice.UpdateAsync(link);
 
         return AcceptedAtAction(nameof(GetById), new { id = link.Id }, link);
     }
@@ -97,14 +97,14 @@ public class LinksController(ILinkRepository linkRepository, IMapper mapper) : C
     [ProducesResponseType(typeof(LinkViewModel), StatusCodes.Status202Accepted)]
     public async Task<IActionResult> Delete(Guid id)
     {
-        var linkInDb = await _linkRepository.FindByIdAsync(id);
+        var linkInDb = await _linkSerice.FindByIdAsync(id);
 
         if (linkInDb == null)
         {
             return BadRequest();
         }
 
-        await _linkRepository.DeleteAsync(linkInDb);
+        await _linkSerice.DeleteAsync(linkInDb);
 
         return AcceptedAtAction(nameof(GetById), new { id = linkInDb.Id }, linkInDb);
     }
