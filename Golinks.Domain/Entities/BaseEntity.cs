20ccdc6ce1f4f205a13bdf96ba63852f3975ba6a
Jonas Lima de Amorim <dev.amorim@outlook.com>
2026-06-17 02:12:29 -0300

    feat: generate entity IDs in application layer using UUID v7

--- Golinks.Domain/Entities/BaseEntity.cs
+++ Golinks.Domain/Entities/BaseEntity.cs
@@ -2,7 +2,7 @@
 
 public abstract class BaseEntity
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.CreateVersion7();
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 }
